cc7bfff049ee44e17f345f1bdebbc13555613106
Oleg Andreev <oleganza@gmail.com>
2009-01-23 10:46:38 +0100

    added ~/bin to PATH if it exists

--- common/path.sh
+++ common/path.sh
@@ -1,2 +1,7 @@
 # All possible system-wide "bin" paths
 export PATH="/usr/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/local/sbin:$PATH"
+
+# set PATH so it includes user's private bin if it exists
+if [ -d ~/bin ] ; then
+    PATH=~/bin:"${PATH}"
+fi
