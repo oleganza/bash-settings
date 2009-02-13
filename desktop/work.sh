7684df3e4b93bd7f6d8704b0941c94f39b37274d
Oleg Andreev <oleganza@gmail.com>
2009-02-13 12:37:30 +0100

    added aliases for OSS/io, OSS/ruby

--- desktop/work.sh
+++ desktop/work.sh
@@ -11,4 +11,9 @@ alias orange='  work;   cd orange'
 alias or='      orange'
 alias mn='      orange; cd metanymph.git'
 
+alias oss='     cd ~/OSS'
+alias cdio='    oss; cd io'
+alias cdrb='    oss; cd ruby'
+alias cdruby='  cdrb'
+
 alias .bash='   cd ~/.bash'
