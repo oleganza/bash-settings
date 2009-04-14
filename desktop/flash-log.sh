99eb4087a89b297b7359372224dcb20a2a38ed2f
Oleg Andreev <oleganza@gmail.com>
2009-04-14 11:03:17 +0200

    FLOG variable added

--- desktop/flash-log.sh
+++ desktop/flash-log.sh
@@ -1,2 +1,3 @@
-alias flog='tail -f ~/Library/Preferences/Macromedia/Flash\ Player/Logs/flashlog.txt'
+export FLOG=~/Library/Preferences/Macromedia/Flash\ Player/Logs/flashlog.txt
+alias flog='tail -f "$FLOG"'
 alias flcfg='$ASYNC_EDITOR /Library/Application\ Support/Macromedia/mm.cfg'
