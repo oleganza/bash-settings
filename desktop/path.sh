79227b45661493470ec78522acf1ee680c72cdbc
Oleg Andreev <oleganza@gmail.com>
2010-01-15 23:57:59 +0100

    move to local ~/.gem folder

--- desktop/path.sh
+++ desktop/path.sh
@@ -2,3 +2,4 @@
 export PATH="/usr/local/jruby/bin:$PATH"             # jruby
 export PATH="/usr/local/mysql/bin:$PATH"             # mysql
 export PATH="/opt/local/lib/postgresql83/bin:$PATH"  # postgresql
+export PATH="~/.gem/ruby/1.8/bin:$PATH" # local gems
