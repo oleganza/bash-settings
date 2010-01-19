76f8806ae6a6ab27d1cefcb257ae2e781f0f9f3e
Oleg Andreev <oleganza@gmail.com>
2010-01-19 14:01:35 +0100

    android and go

--- desktop/misc-env.sh
+++ desktop/misc-env.sh
@@ -7,3 +7,5 @@ export PGDATA="/opt/local/var/db/postgresql83/defaultdb"
 export VKONTAKTE_EMAIL="oleganza@gmail.com"
 
 export GEMS='/Library/Ruby/Gems/1.8/gems'
+
+export ANDROID_SDK="/Users/oleganza/Work/android/android-sdk-mac"
--- desktop/path.sh
+++ desktop/path.sh
@@ -2,3 +2,6 @@
 export PATH="/usr/local/jruby/bin:$PATH"             # jruby
 export PATH="/usr/local/mysql/bin:$PATH"             # mysql
 export PATH="/opt/local/lib/postgresql83/bin:$PATH"  # postgresql
+export GOROOT="$HOME/OSS/go/golang"                  # golang
+export GOBIN="$HOME/OSS/go/golang/bin"               # golang
+export PATH="$GOBIN:$PATH"                           # golang
