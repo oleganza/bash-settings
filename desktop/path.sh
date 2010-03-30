511598ebb61b325ed8084b42baa8f1fdf8af2bd7
Oleg Andreev <oleganza@gmail.com>
2010-03-30 11:33:39 +0200

    make the gem install shut up about bin path

--- desktop/path.sh
+++ desktop/path.sh
@@ -2,4 +2,4 @@
 export PATH="/usr/local/jruby/bin:$PATH"             # jruby
 export PATH="/usr/local/mysql/bin:$PATH"             # mysql
 export PATH="/opt/local/lib/postgresql83/bin:$PATH"  # postgresql
-export PATH="~/.gem/ruby/1.8/bin:$PATH"              # local gems
+export PATH="/Users/$USER/.gem/ruby/1.8/bin:$PATH"              # local gems
