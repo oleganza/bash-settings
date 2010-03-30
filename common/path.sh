ba16ec1db426fc112d1fab0d071ba5b4e1a86723
Oleg Andreev <oleganza@gmail.com>
2010-03-30 11:22:03 +0200

    removed unnecessary PATHs

--- common/path.sh
+++ common/path.sh
@@ -1,7 +1,5 @@
 # All possible system-wide "bin" paths
-export PATH="/usr/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/local/sbin:$PATH"
-export PATH="/usr/homebrew/bin:$PATH"
-export MANPATH="/usr/homebrew/share/man:$MANPATH"
+#export PATH="/usr/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/local/sbin:$PATH"
 
 # set PATH so it includes user's private bin if it exists
 if [ -d ~/bin ] ; then
