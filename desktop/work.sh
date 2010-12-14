76968c904e27d3d8e7853647a76d4d74aed463c7
Oleg Andreev <oleganza@gmail.com>
2010-12-14 07:33:54 +0100

    work aliases upd

--- desktop/work.sh
+++ desktop/work.sh
@@ -1,21 +1,2 @@
-alias mate_mml='mate .git/config app features bin benchmarks doc lib config spinoffs spec README public .gitignore  Rakefile vendor tools merb'
-alias cdmml='cd ~/Work/mml/mml2.git'
-alias mml='cdmml && mate_mml'
-
-alias work='    cd ~/Work'
-alias pierlis=' work;   cd pierlis'
-alias pl='      pierlis'
-alias spidart=' work;   cd spidart'
-alias nuage='   work;   cd nuage'
-alias orange='  work;   cd orange'
-alias or='      orange'
-alias mn='      orange; cd metanymph.git'
-
-alias oss='     cd ~/OSS'
-alias cdio='    oss; cd io'
-alias cdrb='    oss; cd ruby'
-alias cdruby='  cdrb'
-
-alias .bash='   cd ~/.bash'
-
 alias ru='touch tmp/restart.txt' # stands for 'rack up': reloads rack (merb) app
+alias xcode='open *.xcodeproj'
