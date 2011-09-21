5f30e07a70013e546544a76ad4f51755a490ffe1
Oleg Andreev <oleganza@gmail.com>
2011-09-21 15:08:31 +0200

    fixed path; added trash() function

--- desktop/work.sh
+++ desktop/work.sh
@@ -1,2 +1,9 @@
 alias ru='touch tmp/restart.txt' # stands for 'rack up': reloads rack (merb) app
 alias xcode='open *.xcodeproj'
+
+function trash {
+for b in "$@"
+do
+osascript -e "tell app \"Finder\" to delete POSIX file \"${PWD}/$b\"" 1>/dev/null
+done
+}
