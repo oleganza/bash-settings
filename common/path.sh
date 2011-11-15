ad69075cff88a6c9efccf474b3dedc629eabdd59
Oleg Andreev <oleganza@gmail.com>
2011-11-15 11:39:28 +0100

    added spark from @holman

--- common/path.sh
+++ common/path.sh
@@ -1,6 +1,6 @@
 # All possible system-wide "bin" paths
 #export PATH="/usr/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/local/sbin:$PATH"
-export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/local/bin:~/.bash/bin:$PATH"
 
 # set PATH so it includes user's private bin if it exists
 if [ -d ~/bin ] ; then
