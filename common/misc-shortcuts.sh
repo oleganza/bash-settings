a085d478b0eaf34cdecce6e198a2ceea7d424331
Oleg Andreev <oleganza@gmail.com>
2011-12-15 12:04:46 +0100

    disabled cycling completion

--- common/misc-shortcuts.sh
+++ common/misc-shortcuts.sh
@@ -26,4 +26,5 @@ alias mgem="cd \$GEMS; mate \$1"
 alias sgi32="env ARCHFLAGS=\"-Os -arch i386 -fno-common\" gem install --no-ri --no-rdoc"
 alias sgi64="env ARCHFLAGS=\"-Os -arch x86_64 -fno-common\" gem install --no-ri --no-rdoc"
 
-bind '"\t":menu-complete'
+# Cycling tab-completion. Disabling because it's more annoying than without it.
+# bind '"\t":menu-complete'
