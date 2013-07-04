3b726b0f4d273d3d551e24bf78d70bc54e2f22bb
Oleg Andreev <oleganza@gmail.com>
2013-07-04 10:17:42 +0200

    added pebble to PATH

--- desktop/path.sh
+++ desktop/path.sh
@@ -3,3 +3,4 @@ export PATH="/usr/local/jruby/bin:$PATH"             # jruby
 export PATH="/usr/local/mysql/bin:$PATH"             # mysql
 export PATH="/opt/local/lib/postgresql83/bin:$PATH"  # postgresql
 export PATH="/Users/$USER/.gem/ruby/1.8/bin:$PATH"              # local gems
+export PATH=~/pebble-dev/arm-cs-tools/bin:$PATH
