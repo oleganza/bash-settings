9eb6e300b232f12a5482f7fd8628c20411311b14
Oleg Andreev <oleganza@gmail.com>
2014-11-24 11:16:47 +0100

    misc path fixes

--- common/path.sh
+++ common/path.sh
@@ -1,6 +1,6 @@
 # All possible system-wide "bin" paths
 #export PATH="/usr/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/local/sbin:$PATH"
-export PATH="/usr/local/bin:~/.bash/bin:$PATH"
+export PATH="/usr/local/bin:~/.bash/bin:~/pebble-dev/PebbleSDK-2.0.0/bin:$PATH"
 
 # set PATH so it includes user's private bin if it exists
 if [ -d ~/bin ] ; then
--- desktop/path.sh
+++ desktop/path.sh
@@ -1,6 +1,8 @@
 
+export PATH="/usr/local/sbin:$PATH"                  # homebrew
 export PATH="/usr/local/jruby/bin:$PATH"             # jruby
 export PATH="/usr/local/mysql/bin:$PATH"             # mysql
 export PATH="/opt/local/lib/postgresql83/bin:$PATH"  # postgresql
-export PATH="/Users/$USER/.gem/ruby/1.8/bin:$PATH"              # local gems
+export PATH="/Users/$USER/.gem/ruby/1.8/bin:$PATH"   # local gems
 export PATH=~/pebble-dev/arm-cs-tools/bin:$PATH
+export PATH=~/pebble-dev/PebbleSDK-2.0.0/bin:$PATH
