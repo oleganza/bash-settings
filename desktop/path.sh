0e46de239fc0b221ae7bb3d2decc9bac88011365
Oleg Andreev <oleganza@gmail.com>
2015-05-12 11:20:47 +0200

    go path

--- desktop/path.sh
+++ desktop/path.sh
@@ -6,3 +6,6 @@ export PATH="/opt/local/lib/postgresql83/bin:$PATH"  # postgresql
 export PATH="/Users/$USER/.gem/ruby/1.8/bin:$PATH"   # local gems
 export PATH=~/pebble-dev/arm-cs-tools/bin:$PATH
 export PATH=~/pebble-dev/PebbleSDK-2.0.0/bin:$PATH
+
+export GOPATH=$HOME/Code/go
+export PATH=$PATH:$GOPATH/bin
